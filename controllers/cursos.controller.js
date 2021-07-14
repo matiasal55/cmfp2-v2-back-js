fd2fdb4530fd1b22c3aae614f50e8ce9fec9d42a
Matias <matiasalarcon55@hotmail.com>
2021-07-14 16:15:19 -0300

    Envio de array directamente y no dentro de un objeto

--- controllers/cursos.controller.js
+++ controllers/cursos.controller.js
@@ -2,7 +2,7 @@ const { cursosList } = require('../services/cursos.services');
 
 const getCursos = async (req, res) => {
     const cursos = await cursosList();
-    res.json({ cursos });
+    res.json(cursos);
 };
 
 module.exports = { getCursos };
