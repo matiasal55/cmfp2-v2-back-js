278b43ecb6778e7551bd995d8229a8ada1f1945c
Matias <matiasalarcon55@hotmail.com>
2021-07-15 15:56:17 -0300

    Se agrego el -1 para que el resultado se obtenga a partir de la noticia mas reciente

--- repositories/noticias.repository.js
+++ repositories/noticias.repository.js
@@ -1,12 +1,12 @@
 const Model = require('../models/Noticia');
 
 const getNoticias = async () => {
-    const noticias = Model.find();
+    const noticias = Model.find().sort({ _id: -1 });
     return noticias;
 };
 
 const getUltimasNoticias = async () => {
-    const noticias = Model.find().sort({ _id: 1 }).limit(3);
+    const noticias = Model.find().sort({ _id: -1 }).limit(3);
     return noticias;
 };
 
