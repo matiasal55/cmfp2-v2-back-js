bb346a5e3cfac6db13001b05882d41695b4fc81e
Matias <matiasalarcon55@hotmail.com>
2021-07-15 16:50:03 -0300

    Implementacion de try catch en cursos y noticias controllers

--- controllers/noticias.controller.js
+++ controllers/noticias.controller.js
@@ -1,19 +1,36 @@
 const { noticiasList, noticiasLastItems, noticia } = require('../services/noticias.services');
+const { success } = require('../services/messages/success');
+const { error, internal } = require('../services/messages/error');
 
 const getNoticias = async (req, res) => {
-    const noticias = await noticiasList();
-    res.json(noticias);
+    try {
+        const noticias = await noticiasList();
+        if (noticias.length > 0) return success(res, noticias);
+        return error(res, 'No se encontraron noticias');
+    } catch (e) {
+        return internal(res);
+    }
 };
 
 const getUltimasNoticias = async (req, res) => {
-    const noticias = await noticiasLastItems();
-    res.json(noticias);
+    try {
+        const noticias = await noticiasLastItems();
+        if (noticias.length > 0) return res.json(noticias);
+        return error(res, 'No se encontraron noticias');
+    } catch (e) {
+        return internal(res);
+    }
 };
 
 const getNoticia = async (req, res) => {
-    const { id } = req.params;
-    const request = await noticia(id);
-    res.json(request);
+    try {
+        const { id } = req.params;
+        const request = await noticia(id);
+        if (request) return res.json(request);
+        return error(res, 'No se encontró la noticia con ese id');
+    } catch (e) {
+        return internal(res);
+    }
 };
 
 module.exports = { getNoticias, getUltimasNoticias, getNoticia };
--- services/messages/error.js
+++ services/messages/error.js
@@ -1,3 +1,7 @@
 exports.error = (res, message, status = 400) => {
     res.status(status).json({ message });
 };
+
+exports.internal = (res) => {
+    res.status(500).json({ message: 'Error interno' });
+};
