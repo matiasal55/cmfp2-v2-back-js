adc4245855788f895447af0109a6eb37cd58796f
Matias <matiasalarcon55@hotmail.com>
2021-07-15 20:15:23 -0300

    Correccion de ruta de controller de taller

--- routes/cursos/taller.routes.js
+++ routes/cursos/taller.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getTalleres, getTaller } = require('../../controllers/cursos.controller');
+const { getTalleres, getTaller } = require('../../controllers/cursos/taller.controller');
 
 router.get('/', getTalleres);
 router.get('/:id', getTaller);
